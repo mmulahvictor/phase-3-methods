80646293716222f0e3b50fca1ce76c7709ee5fc6
mmulahvictor <mmulahvictor@gmail.com>
2022-08-11 02:21:14 +0300

    commit tree 7f0381de253177ac87103d633976614d09ac867f

--- methods.rb
+++ methods.rb
@@ -1,1 +1,27 @@
-# Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+greet_programmer
+
+def greet name
+    puts "Hello, #{name}!"
+end
+greet "Naureen"
+
+def greet_with_default name = "programmer"
+    puts "Hello, #{name}!"
+end
+
+def add num1, num2
+    return num1 + num2
+end
+sum = add(1, 2)
+
+
+
+#halve returns half of the number and nil if the number is not a number
+
+def halve num
+    return nil if num.is_a? String
+    return num / 2
+end
